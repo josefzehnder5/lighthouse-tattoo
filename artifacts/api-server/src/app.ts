f4a5ca85142fb885b2cc368ba3b0b9f047105213
josefzehnder5 <josefzehnder5@gmail.com>
2026-05-16 16:24:24 -0500

    Add Request and Response types from express

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,4 +1,8 @@
-import express, { type Express } from "express";
+import express, {
+  type Express,
+  type Request,
+  type Response,
+} from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import router from "./routes";
